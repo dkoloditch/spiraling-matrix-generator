74ec73b17f751dcd6df02fa672c7b3d569a3a863
Derek Koloditch <derek.koloditch@gmail.com>
2016-01-24 13:22:39 -0700

    some cleanup

--- spiraling_matrix.rb
+++ spiraling_matrix.rb
@@ -1,9 +1,26 @@
-@matrix = []
-puts "Enter a number: "
-user_provided_number = gets.chomp.to_i
+def run
+  @matrix = []
+  puts "Enter a number: "
+  @user_provided_number = gets.chomp.to_i
 
-def base_row_finished?
-  @matrix.size == 1 && @matrix.first.size == 2
+  (@user_provided_number + 1).times do |n|
+    set_current_direction
+    create_row_if_needed
+
+    if @current_direction == "right"
+      @matrix.first.push(n)
+    elsif @current_direction == "left"
+      @matrix.last.unshift(n)
+    elsif @current_direction == "up"
+      # add the number to array after the one that contains the previous number
+      previous_row_idx = @matrix.reverse.map.with_index{|row, idx| idx if row.include?(n-1)}.compact.first
+      @matrix.reverse[previous_row_idx + 1].unshift(n)
+    elsif @current_direction == "down"
+      # add the number to array after the one that contains the previous number
+      previous_row_idx = @matrix.map.with_index{|row, idx| idx if row.include?(n-1)}.compact.first
+      @matrix[previous_row_idx + 1].push(n)
+    end
+  end
 end
 
 def set_current_direction
@@ -23,10 +40,6 @@ def set_current_direction
   end
 end
 
-def all_row_sizes_are_equal?
-  @matrix.collect{|row| row.size}.uniq.size <= 1
-end
-
 def create_row_if_needed
   if @matrix.empty?
     @matrix << []
@@ -39,33 +52,26 @@ def create_row_if_needed
   end
 end
 
-(user_provided_number + 1).times do |n|
-  set_current_direction
-  create_row_if_needed
+def base_row_finished?
+  @matrix.size == 1 && @matrix.first.size == 2
+end
 
-  if @current_direction == "right"
-    @matrix.first.push(n)
-  elsif @current_direction == "left"
-    @matrix.last.unshift(n)
-  elsif @current_direction == "up"
-    # add the number to array after the one that contains the previous number
-    previous_row_idx = @matrix.reverse.map.with_index{|row, idx| idx if row.include?(n-1)}.compact.first
-    @matrix.reverse[previous_row_idx + 1].unshift(n)
-  elsif @current_direction == "down"
-    # add the number to array after the one that contains the previous number
-    previous_row_idx = @matrix.map.with_index{|row, idx| idx if row.include?(n-1)}.compact.first
-    @matrix[previous_row_idx + 1].push(n)
+def all_row_sizes_are_equal?
+  @matrix.collect{|row| row.size}.uniq.size <= 1
+end
+
+def output_results
+  results = @matrix.map do |row|
+    row.collect do |n|
+      spaces = " " * ((@user_provided_number.to_s.size - n.to_s.size))
+      n.to_s + spaces
+    end
   end
+
+  puts nil
+  results.each{|row| puts row.join(' ')}
 end
 
 
-puts nil
-
-results = @matrix.map do |row|
-  row.collect do |n|
-    spaces = " " * ((user_provided_number.to_s.size - n.to_s.size))
-    n.to_s + spaces
-  end
-end
-
-results.each{|row| puts row.join(' ')}
+run
+output_results
